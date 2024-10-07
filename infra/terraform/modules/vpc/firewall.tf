19080a31732038e09b28e1937041f8e4457da78a
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 00:32:19 +0800

    infra: add rule to enable internal traffic

--- infra/terraform/modules/vpc/firewall.tf
+++ infra/terraform/modules/vpc/firewall.tf
@@ -11,3 +11,23 @@ resource "google_compute_firewall" "game_instances_allow_ssh" {
   }
   target_tags = ["game-instance"]
 }
+
+resource "google_compute_firewall" "game_instances_allow_internal" {
+  name        = "game-instances-allow-internal"
+  network     = google_compute_network.game_network.name
+  description = "Allow internal connections between game instances"
+
+  disabled           = true # Disable internal traffic by default, they will be enabled by the game coordinator
+  source_ranges      = ["10.124.0.0/16"]
+  destination_ranges = ["10.124.0.0/16"]
+  direction          = "INGRESS"
+  allow {
+    protocol = "all"
+  }
+
+  target_tags = ["game-instance"]
+
+  lifecycle {
+    ignore_changes = [disabled] # Ignore changes to the disabled attribute, which is managed by the game coordinator
+  }
+}
