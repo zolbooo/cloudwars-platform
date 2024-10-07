854cccbcdf156b6f4e4e564c20210d0d4316ad9f
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 00:44:46 +0800

    infra: allow to connect to instances via SSH from everywhere

--- infra/terraform/modules/vpc/firewall.tf
+++ infra/terraform/modules/vpc/firewall.tf
@@ -1,9 +1,9 @@
 resource "google_compute_firewall" "game_instances_allow_ssh" {
   name        = "game-instances-allow-ssh"
   network     = google_compute_network.game_network.name
-  description = "Allow SSH connections from IAP to game instances"
+  description = "Allow SSH connections to game instances"
 
-  source_ranges = ["35.235.240.0/20"]
+  source_ranges = ["0.0.0.0/0"]
   direction     = "INGRESS"
   allow {
     protocol = "tcp"
