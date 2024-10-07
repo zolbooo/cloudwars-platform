8daca423a6911101ac3b06fc92a61e3d54803388
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 01:04:31 +0800

    infra: define KMS key version viewer IAM role

---
+++ infra/terraform/modules/game-coordinator/iam.tf
@@ -0,0 +1,7 @@
+resource "google_project_iam_custom_role" "key_version_viewer" {
+  role_id = "key_version_viewer"
+  title   = "Cloud KMS Key version viewer"
+  permissions = [
+    "cloudkms.cryptoKeyVersions.list",
+  ]
+}
