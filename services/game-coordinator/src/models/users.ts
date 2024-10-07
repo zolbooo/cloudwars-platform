8969f12b61c1cfe6781d79cbdf70fa918393475c
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 01:21:10 +0800

    feat(game-coordinator): implement basic user model

---
+++ services/game-coordinator/src/models/users.ts
@@ -0,0 +1,53 @@
+import { firestore } from "./db";
+
+export interface User {
+  id: string;
+  role: "admin" | "user";
+  username: string;
+  passwordHash: string;
+  teamId: string | null;
+}
+
+export interface IUserModel {
+  rootUserExists(): Promise<boolean>;
+  createRootUser(
+    input: Omit<User, "id" | "role" | "teamId">
+  ): Promise<
+    | { success: true; id: string }
+    | { success: false; error: "root_user_already_exists" }
+  >;
+}
+
+class UserModel implements IUserModel {
+  async rootUserExists() {
+    const snapshot = await firestore
+      .collection("users")
+      .where("role", "==", "admin")
+      .get();
+    return snapshot.size > 0;
+  }
+
+  async createRootUser(
+    input: Omit<User, "id" | "role" | "teamId">
+  ): Promise<
+    | { success: true; id: string }
+    | { success: false; error: "root_user_already_exists" }
+  > {
+    return await firestore.runTransaction(async () => {
+      const rootUserExists = await this.rootUserExists();
+      if (rootUserExists) {
+        return { success: false, error: "root_user_already_exists" };
+      }
+
+      const userRef = firestore.collection("users").doc();
+      await userRef.set({
+        ...input,
+        id: userRef.id,
+        role: "admin",
+      });
+      return { success: true, id: userRef.id };
+    });
+  }
+}
+
+export const users: IUserModel = new UserModel();
