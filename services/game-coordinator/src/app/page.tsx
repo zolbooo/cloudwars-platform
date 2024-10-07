1bf44e93f4580d6235b59a276327e66a0d3b14a0
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 01:56:47 +0800

    feat(home): navigate to setup pages

--- services/game-coordinator/src/app/page.tsx
+++ services/game-coordinator/src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div></div>;
+import { redirect } from "next/navigation";
+
+import { users } from "@/models/users";
+import { metadata } from "@/models/metadata";
+
+export default async function Home() {
+  const rootUserExists = await users.rootUserExists();
+  if (!rootUserExists) {
+    return redirect("/setup/root-user");
+  }
+  const gameSettings = await metadata.getGameSettings();
+  if (gameSettings === null) {
+    return redirect("/setup/game-settings");
+  }
+  return null;
 }
