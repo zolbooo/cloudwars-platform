6302f5796c76aac9c5c9ac3bb8b7f70fa8b5217f
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 10:32:36 +0800

    infra: enable Compute Engine API if not already enabled

--- infra/terraform/modules/vpc/network.tf
+++ infra/terraform/modules/vpc/network.tf
@@ -1,6 +1,13 @@
+resource "google_project_service" "compute-api" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+}
+
 resource "google_compute_network" "game_network" {
   project = var.project_id
   name    = "game-network"
 
   auto_create_subnetworks = false
+
+  depends_on = [google_project_service.compute-api]
 }
