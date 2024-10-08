aa9efcc1b80e5e0cc0398d753e126a167bab24aa
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 10:32:23 +0800

    infra: enable Firestore API if not enabled

--- infra/terraform/modules/db/db.tf
+++ infra/terraform/modules/db/db.tf
@@ -1,6 +1,12 @@
+resource "google_project_service" "firestore-api" {
+  service = "firestore.googleapis.com"
+}
+
 resource "google_firestore_database" "game_database" {
   project     = var.project_id
   name        = "(default)"
   location_id = var.region
   type        = "FIRESTORE_NATIVE"
+
+  depends_on = [google_project_service.firestore-api]
 }
