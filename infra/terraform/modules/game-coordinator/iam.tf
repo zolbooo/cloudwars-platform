7cdd630cef91d247ced9624ab331f32203a3e299
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 12:04:47 +0800

    infra: define a checker dispatcher service account

--- infra/terraform/modules/game-coordinator/iam.tf
+++ infra/terraform/modules/game-coordinator/iam.tf
@@ -9,3 +9,7 @@ resource "google_project_iam_custom_role" "key_version_viewer" {
 resource "google_service_account" "game_coordinator" {
   account_id = "game-coordinator"
 }
+resource "google_service_account" "dispatcher" {
+  account_id  = "dispatcher"
+  description = "Service account used by the checker dispatcher"
+}
---
+++ infra/terraform/modules/game-coordinator/outputs.tf
@@ -0,0 +1,3 @@
+output "checker_service_account_email" {
+  value = google_service_account.dispatcher.email
+}
