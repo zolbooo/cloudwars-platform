cf6638f1366efbeed820327459b86316ed93c1f4
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 22:50:19 +0800

    fix(example-service-checker): pass missing arguments to report_to_coordinator function

--- checkers/example-service/checker/__init__.py
+++ checkers/example-service/checker/__init__.py
@@ -3,5 +3,5 @@ from .report import report_to_coordinator
 
 
 def run_checker(target_ip: str, round_flag: str):
-    service_status = check(target_ip, round_flag)
-    report_to_coordinator(service_status)
+    service_status = check(target_ip=target_ip, round_flag=round_flag)
+    report_to_coordinator(service_status=service_status, round_flag=round_flag)
