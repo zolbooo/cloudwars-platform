820bb88ba1605326d9f39383a70f00d60725df52
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 10:46:55 +0800

    fix(game-coordinator): force home page to be rendered only by the server

--- services/game-coordinator/src/app/page.tsx
+++ services/game-coordinator/src/app/page.tsx
@@ -3,6 +3,10 @@ import { redirect } from "next/navigation";
 import { users } from "@/models/users";
 import { metadata } from "@/models/metadata";
 
+// Force Next.js to treat this route as server-side rendered
+// Without this line, during the build process, Next.js will treat this route as static and build a static HTML file for it
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const rootUserExists = await users.rootUserExists();
   if (!rootUserExists) {
