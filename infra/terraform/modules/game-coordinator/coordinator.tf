3d24618a673554abd17c23a081ca8d250f00b85e
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 13:04:03 +0800

    feat(game-coordinator): specify environment variables for flag singing key

--- infra/terraform/modules/game-coordinator/coordinator.tf
+++ infra/terraform/modules/game-coordinator/coordinator.tf
@@ -15,6 +15,23 @@ resource "google_cloud_run_v2_service" "game_coordinator" {
     service_account = google_service_account.game_coordinator.email
     containers {
       image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.app.name}/game-coordinator:latest"
+
+      env {
+        name  = "GCP_PROJECT_ID"
+        value = var.project_id
+      }
+      env {
+        name  = "GAME_KEY_RING_NAME"
+        value = google_kms_key_ring.game_key_ring.name
+      }
+      env {
+        name  = "FLAG_KEY_NAME"
+        value = google_kms_crypto_key.flag_key.name
+      }
+      env {
+        name  = "FLAG_KEY_REGION"
+        value = google_kms_key_ring.game_key_ring.location
+      }
     }
   }
 
