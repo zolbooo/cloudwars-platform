6aae189bbf6c276a5460881e5db02542a594532b
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-08 12:24:10 +0800

    infra: create a subnet for the checker

--- infra/terraform/modules/vpc/network.tf
+++ infra/terraform/modules/vpc/network.tf
@@ -11,3 +11,12 @@ resource "google_compute_network" "game_network" {
 
   depends_on = [google_project_service.compute-api]
 }
+
+resource "google_compute_subnetwork" "checker_subnet" {
+  project = var.project_id
+  region  = var.region
+
+  name          = "checker-subnet"
+  ip_cidr_range = "10.124.255.0/24"
+  network       = google_compute_network.game_network.name
+}
--- infra/terraform/modules/vpc/outputs.tf
+++ infra/terraform/modules/vpc/outputs.tf
@@ -1,3 +1,6 @@
 output "game_network_name" {
   value = google_compute_network.game_network.name
 }
+output "checker_subnet_name" {
+  value = google_compute_subnetwork.checker_subnet.name
+}
