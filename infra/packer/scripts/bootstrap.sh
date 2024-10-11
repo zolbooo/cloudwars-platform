fd64becfc0843e9b68615050db44be950605cd36
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-11 15:07:05 +0800

    fix(bootstrap): submit SSH private key to the correct secret

--- infra/packer/scripts/bootstrap.sh
+++ infra/packer/scripts/bootstrap.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-sudo useradd -m -s /bin/bash game
-sudo usermod -aG docker game
-echo 'game ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/game
-
 sudo -u game mkdir -p /home/game/.ssh
 sudo -u game ssh-keygen -t ed25519 -f /home/game/.ssh/instance.key -N ""
 sudo -u game mv /home/game/.ssh/{instance.key.pub,authorized_keys}
 sudo -u game chmod -R 700 /home/game/.ssh
 
 SSH_KEY_SECRET_NAME=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ssh-key-secret-name" -H "Metadata-Flavor: Google")
-sudo -u game gcloud secrets versions add team-1-ssh-key --data-file=/home/game/.ssh/instance.key
+sudo -u game gcloud secrets versions add $SSH_KEY_SECRET_NAME --data-file=/home/game/.ssh/instance.key
 sudo -u game shred -u /home/game/.ssh/instance.key
 
 # See: https://stackoverflow.com/questions/8981164/self-deleting-shell-script/34303677#34303677
