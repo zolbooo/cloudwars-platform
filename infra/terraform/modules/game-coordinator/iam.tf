31adee5711cc169236518f96f8d8242c9141b2bf
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-11 13:39:09 +0800

    infra: allow game coordinator to edit firewall rules

--- infra/terraform/modules/game-coordinator/iam.tf
+++ infra/terraform/modules/game-coordinator/iam.tf
@@ -14,3 +14,22 @@ resource "google_project_iam_member" "game_coordinator_instance-lister" {
   role    = "roles/compute.viewer"
   member  = "serviceAccount:${var.game_coordinator_service_account_email}"
 }
+
+resource "google_project_iam_custom_role" "firewall_editor" {
+  role_id     = "firewall_editor"
+  title       = "Firewall Editor"
+  description = "Allows updating a firewall rule"
+  permissions = [
+    "compute.firewalls.update",
+  ]
+}
+resource "google_project_iam_member" "game_coordinator-firewall_editor" {
+  project = var.project_id
+  role    = google_project_iam_custom_role.firewall_editor.id
+  member  = "serviceAccount:${var.game_coordinator_service_account_email}"
+  condition {
+    title       = "Only allow updates to the game instances firewall"
+    description = "Only allow updates to the game instances firewall"
+    expression  = "resource.name.startsWith(\"projects/${var.project_id}/global/firewalls/game-instances-\")"
+  }
+}
