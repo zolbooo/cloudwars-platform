0bed1c1197a9a7c7903cc78fa94e019c4f81241d
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-15 00:53:39 +0800

    infra(game-coordinator): add background tasks queue

---
+++ infra/terraform/modules/game-coordinator/background-tasks.tf
@@ -0,0 +1,42 @@
+resource "google_project_service" "cloudtasks" {
+  project            = var.project_id
+  service            = "cloudtasks.googleapis.com"
+  disable_on_destroy = true
+}
+
+resource "google_cloud_tasks_queue" "game_background_tasks" {
+  name     = "game-background-tasks-queue"
+  location = var.region
+
+  depends_on = [
+    google_project_service.cloudtasks
+  ]
+}
+
+resource "google_cloud_tasks_queue_iam_binding" "game_background_tasks_enqueuer" {
+  project  = var.project_id
+  location = var.region
+  name     = google_cloud_tasks_queue.game_background_tasks.name
+  role     = "roles/cloudtasks.enqueuer"
+  members = [
+    "serviceAccount:${var.game_coordinator_service_account_email}",
+  ]
+}
+resource "google_cloud_tasks_queue_iam_binding" "game_background_tasks_task_delete" {
+  project  = var.project_id
+  location = var.region
+  name     = google_cloud_tasks_queue.game_background_tasks.name
+  role     = "roles/cloudtasks.taskDeleter"
+  members = [
+    "serviceAccount:${var.game_coordinator_service_account_email}",
+  ]
+}
+resource "google_cloud_tasks_queue_iam_binding" "game_background_tasks_viewer" {
+  project  = var.project_id
+  location = var.region
+  name     = google_cloud_tasks_queue.game_background_tasks.name
+  role     = "roles/cloudtasks.viewer"
+  members = [
+    "serviceAccount:${var.game_coordinator_service_account_email}",
+  ]
+}
