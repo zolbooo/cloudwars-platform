0ffa99ace5ccae86ac1bc64cde86bb190eb47792
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-16 12:17:25 +0800

    refactor(teams-model): remove validation logic in the create method

--- services/game-coordinator/src/models/teams.ts
+++ services/game-coordinator/src/models/teams.ts
@@ -2,8 +2,6 @@ import { z } from "zod";
 import { FieldValue } from "@google-cloud/firestore";
 
 import { firestore } from "./db";
-import { metadata } from "./metadata";
-import { users } from "./users";
 
 export const serviceStatusSchema = z.enum(["UP", "MUMBLE", "CORRUPT", "DOWN"]);
 export type ServiceStatus = z.infer<typeof serviceStatusSchema>;
@@ -24,21 +22,10 @@ export interface Team {
 }
 
 export interface ITeamsModel {
+  create(input: Omit<Team, "score" | "serviceStatus">): Promise<void>;
+
   getTeamById(id: number): Promise<Team | null>;
   listTeams(): Promise<Omit<Team, "teamPasswordHash">[]>;
-  createTeam(
-    userId: string,
-    input: Omit<Team, "id" | "index" | "score" | "serviceStatus">
-  ): Promise<
-    | { success: true; teamId: number }
-    | {
-        success: false;
-        error:
-          | "team_name_already_taken"
-          | "max_teams_reached"
-          | "setup_not_finished";
-      }
-  >;
 
   updateScore(teamId: number, scores: Team["score"]): Promise<void>;
   updateServiceStatus(
@@ -71,50 +58,21 @@ class TeamsModel implements ITeamsModel {
     });
   }
 
-  async createTeam(
-    userId: string,
-    input: Omit<Team, "id" | "index" | "score">
-  ) {
-    return await firestore.runTransaction(async () => {
-      const config = await metadata.getGameSettings();
-      if (!config) {
-        return { success: false, error: "setup_not_finished" } as const;
-      }
-      const totalTeams = await firestore
-        .collection("teams")
-        .get()
-        .then((snapshot) => snapshot.size);
-      if (totalTeams >= config.maxTeams) {
-        return { success: false, error: "max_teams_reached" } as const;
-      }
-
-      const snapshot = await firestore
-        .collection("teams")
-        .where("name", "==", input.name)
-        .get();
-      if (!snapshot.empty) {
-        return { success: false, error: "team_name_already_taken" } as const;
-      }
-
-      const id = totalTeams + 1;
-      await firestore
-        .collection("teams")
-        .doc(id.toString())
-        .create({
-          ...input,
-          id,
-          score: {
-            total: 0,
-            attack: 0,
-            defense: 0,
-            availability: 0,
-          },
-          serviceStatus: {},
-        });
-
-      await users.edit(userId, { teamId: id });
-      return { success: true, teamId: id } as const;
-    });
+  async create({ id, ...input }: Omit<Team, "score" | "serviceStatus">) {
+    await firestore
+      .collection("teams")
+      .doc(id.toString())
+      .create({
+        ...input,
+        id,
+        score: {
+          total: 0,
+          attack: 0,
+          defense: 0,
+          availability: 0,
+        },
+        serviceStatus: {},
+      });
   }
 
   async updateScore(teamId: number, scores: Team["score"]): Promise<void> {
