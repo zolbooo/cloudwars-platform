a12868ccae890fe272c17f74fbe012838de8fe96
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-21 13:33:29 +0800

    feat: implement getApiKeyUser function

--- services/game-coordinator/src/core/auth/session.ts
+++ services/game-coordinator/src/core/auth/session.ts
@@ -1,9 +1,12 @@
 import "server-only";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
+
+import { User, users } from "@/models/users";
 
 import { TokenClaims } from "./token/schema";
 import { signAuthToken } from "./token/sign";
 import { verifyAuthToken } from "./token/verify";
+import { decodeApiKey, verifyApiKey } from "./api-keys";
 
 const authSessionCookieName =
   process.env.NODE_ENV === "production" ? "__Host-auth" : "auth";
@@ -24,4 +27,30 @@ export async function getSession(): Promise<TokenClaims | null> {
     return null;
   }
   return await verifyAuthToken(tokenCookie.value);
+}
+
+export async function getApiKeyUser(): Promise<User | null> {
+  const authorizationHeader = headers().get("authorization");
+  if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
+    return null;
+  }
+
+  const apiKey = authorizationHeader.split(" ")[1];
+  try {
+    const decodedApiKey = decodeApiKey(apiKey);
+    const user = await users.getById(decodedApiKey.uid);
+    if (!user || !user.apiKeySignature) {
+      return null;
+    }
+    const verifiedApiKey = await verifyApiKey({
+      apiKey,
+      signature: user.apiKeySignature,
+    });
+    if (verifiedApiKey.uid !== user.id) {
+      return null;
+    }
+    return user;
+  } catch {
+    return null;
+  }
 }
