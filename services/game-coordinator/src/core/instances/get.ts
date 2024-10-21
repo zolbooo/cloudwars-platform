386f6549ce4f7d2fda759a172eb84bd59a3af6cc
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2024-10-21 10:47:33 +0800

    feat: implement getGameInstance function

--- services/game-coordinator/src/core/instances/get.ts
+++ services/game-coordinator/src/core/instances/get.ts
@@ -1,34 +1,30 @@
 import compute from "@google-cloud/compute";
 
+import { GameInstance } from "./list";
+
 const instancesClient = new compute.InstancesClient();
 
-export interface GameInstance {
-  name: string;
-  zone: string;
-  publicIP: string;
-  internalIP: string;
-  teamId: number;
-}
-
-export async function listGameInstances() {
+export async function getGameInstance(
+  teamId: number
+): Promise<GameInstance | null> {
   const projectId = await instancesClient.getProjectId();
-  const instances: GameInstance[] = [];
   for await (const [zone, data] of instancesClient.aggregatedListAsync({
     project: projectId,
-    filter: 'labels.purpose = "game-instance" AND labels.team_id:*',
+    filter: `labels.purpose = "game-instance" AND labels.team_id: = ${teamId}`,
+    maxResults: 2,
   })) {
     if (!data.instances) {
       continue;
     }
     for (const instance of data.instances) {
-      instances.push({
+      return {
         name: instance.name!,
         zone: zone.split("/").pop()!,
         publicIP: instance.networkInterfaces![0].accessConfigs![0].natIP!,
         internalIP: instance.networkInterfaces![0].networkIP!,
         teamId: Number(instance.labels!.team_id!),
-      });
+      };
     }
   }
-  return instances;
+  return null;
 }
